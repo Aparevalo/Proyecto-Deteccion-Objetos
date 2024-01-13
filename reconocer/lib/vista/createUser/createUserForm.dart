f4020368ac5c7309700a70c78f4e2cd0df59a1fc
Aparevalo <anarevalovi@uide.com.ec>
2024-01-13 14:10:06 -0500

    Adaptabilidad con el teclado

--- reconocer/lib/vista/createUser/createUserForm.dart
+++ reconocer/lib/vista/createUser/createUserForm.dart
@@ -13,64 +13,87 @@ class CreateUserForm extends StatefulWidget {
 class _CreateUserFormState extends State<CreateUserForm> {
   final UserFormFields formFields = UserFormFields();
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+ bool isVisible = false; // Inicializa la variable isVisible con true
 
-@override
+  @override
+  void initState() {
+    super.initState();
+    KeyboardVisibilityController().onChange.listen((bool visible) {
+      setState(() {
+        isVisible = visible; // Actualiza isVisible según la visibilidad del teclado
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: KeyboardVisibilityBuilder(
-          builder: (context, isKeyboardVisible) {
-            return Column(
-              children: [
-                CustomPaint(
-                  painter: LoginFivePainter(),
-                  child: Container(
-                    height: 100,
-                  ),
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            CustomPaint(
+              painter: LoginFivePainter(),
+              child: Container(
+                height: 100,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: CustomPaint(
+                painter: BottomRegisterFivePainter(),
+                child: Container(
+                  height: 170,
                 ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (!isKeyboardVisible)
-                            Text(
-                              'Registrarse',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          SizedBox(height: 16),
-                          Form(
-                            key: _formKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                formFields.buildNameField(),
-                                formFields.buildLastNameField(),
-                                formFields.buildPhoneField(),
-                                formFields.buildEmailField(),
-                                formFields.buildPasswordField(),
-                                SizedBox(height: 16),
-                                ElevatedButton(
-                                  onPressed: () =>
-                                      UserOperations.createUser(context, _formKey, formFields),
-                                  child: Text('Crear Usuario'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+              ),
+            ),
+            Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      KeyboardVisibilityBuilder(
+                        builder: (context, _isVisible) {
+                          // Muestra el título solo si el teclado no está presente
+                          return isVisible
+                              ? Container() // Puedes usar un contenedor vacío o cualquier otro widget para ocultar el título
+                              : Text(
+                                  'Registrarse',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                        },
                       ),
-                    ),
+                      SizedBox(height: isVisible ? 0 : 14), // Ajusta el tamaño del espacio según la visibilidad del teclado
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            formFields.buildNameField(),
+                            formFields.buildLastNameField(),
+                            formFields.buildPhoneField(),
+                            formFields.buildEmailField(),
+                            formFields.buildPasswordField(),
+                            SizedBox(height: 16),
+                            ElevatedButton(
+                              onPressed: () =>
+                                  UserOperations.createUser(context, _formKey, formFields),
+                              child: Text('Crear Usuario'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            );
-          },
+              ),
+            ),
+          ],
         ),
       ),
     );
